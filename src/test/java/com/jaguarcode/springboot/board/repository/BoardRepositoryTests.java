94356d15007eabc6aab2897bc683141d8523f74d
IKHYEON KIM <behonz@gmail.com>
2021-03-20 13:34:17 +0900

    Member/Board/Reply 엔티티 추가 및 연관관계 테스트케이스 추가

--- src/test/java/com/jaguarcode/springboot/board/repository/BoardRepositoryTests.java
+++ src/test/java/com/jaguarcode/springboot/board/repository/BoardRepositoryTests.java
@@ -5,7 +5,14 @@ import com.jaguarcode.springboot.board.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -27,5 +34,38 @@ public class BoardRepositoryTests {
                     .build();
             boardRepository.save(board);
         });
+    }
+
+    @Test
+    @Transactional
+    public void testRead1() {
+        Optional<Board> result = boardRepository.findById(90L);
+        Board board = result.get();
+        System.out.println(board);
+        System.out.println(board.getWriter());
+    }
+
+    @Test
+    public void testReadWithWriter() {
+        Object result = boardRepository.getBoardWithWriter(90L);
+        Object[] arr = (Object[]) result;
+        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    public void testWithReplyCount() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+        result.get().forEach(row -> {
+            Object[] arr = (Object[]) row;
+            System.out.println(Arrays.toString(arr));
+        });
+    }
+
+    @Test
+    public void testRead3() {
+        Object result = boardRepository.getBoardByBno(90L);
+        Object[] arr = (Object[])result;
+        System.out.println(Arrays.toString(arr));
     }
 }
